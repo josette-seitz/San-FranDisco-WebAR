138146a93e360244adc8a759ce762f856a140ba0
josette-seitz <josetteseitz@gmail.com>
2023-07-26 22:20:08 -0700

    Add Disco Repo

---
+++ Assets/ChangeDiscoBallColors.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDiscoBallColors : MonoBehaviour {
+
+	public List<Material> ballColor;
+	public List<GameObject> discoBall;
+	private int index = 0;
+
+	void OnEnable () {
+
+        StartCoroutine(ChangeDiscoColorFiveSeconds());
+		
+	}
+	
+	IEnumerator ChangeDiscoColorFiveSeconds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            ChangeDiscoColor();
+        }
+    }
+
+    //change disco ball every .5 seconds
+    private void ChangeDiscoColor()
+	{
+        foreach (GameObject disco in discoBall)
+        {
+            disco.GetComponentInChildren<MeshRenderer>().material = ballColor[index];
+        }
+
+        index++;
+
+        if (index == ballColor.Count)
+        {
+            index = 0;
+        }
+    }
+}
---
+++ Assets/RotateBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RotateBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		iTween.RotateBy(this.gameObject, iTween.Hash("y", 2.0f, "time", 10.0f, "easetype", iTween.EaseType.linear));
+	}
+}
